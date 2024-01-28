cf1a38925f03cbe2ce183ec225dc7701e64ee0c8
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2024-01-28 23:36:24 +0100

    Updated Version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,31 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
+
 function myMenuFunction() {
   var menuBtn = document.getElementById("myNavMenu");
+  var menuIcon = document.getElementById("menuIcon");
+  var closeIcon = document.getElementById("closeIcon");
 
   if (menuBtn.className === "nav-menu") {
     menuBtn.className += " responsive";
+    menuIcon.style.display = "none"; // Hide hamburger menu icon
+    closeIcon.style.display = "inline-block"; // Display close icon
   } else {
     menuBtn.className = "nav-menu";
+    menuIcon.style.display = "inline-block"; // Display hamburger menu icon
+    closeIcon.style.display = "none"; // Hide close icon
   }
+
+  // Close the menu when a navigation item is clicked
+  var navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      menuBtn.className = "nav-menu";
+      menuIcon.style.display = "inline-block"; // Display hamburger menu icon
+      closeIcon.style.display = "none"; // Hide close icon
+    });
+  });
 }
+
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
 window.onscroll = function () { headerShadow() };
@@ -33,7 +51,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText", {
-  strings: ["Software Tester", "Traveler", "Aspire Developer"],
+  strings: ["A Software Tester", "ISTQB-Certified", "A Traveler"],
   loop: true,
   typeSpeed: 100,
   backSpeed: 80,
@@ -116,73 +134,37 @@ function scrollActive() {
 
 window.addEventListener('scroll', scrollActive)
 
-
-/* ----- DOWNLOAD CV BUTTON FUNCTIONALITY ----- */
-const downloadCVButtons = document.querySelectorAll('.btn[aria-label="Download CV"]');
-
-downloadCVButtons.forEach(function (button) {
-  button.addEventListener('click', function () {
-    // Replace 'path/to/your/cv.pdf' with the actual path to your CV file
-    const cvFilePath = '/assets/images/Noor-Ismot-Ara-CV.pdf';
-
-    // Create a temporary link element
-    const downloadLink = document.createElement('a');
-
-    // Set the href attribute to the path of your CV file
-    downloadLink.href = cvFilePath;
-
-    // Set the download attribute with the desired file name
-    downloadLink.download = 'Noor-Ismot-Ara-CV.pdf';
-
-    // Append the link to the body
-    document.body.appendChild(downloadLink);
-
-    // Trigger a click event on the link to start the download
-    downloadLink.click();
-
-    // Remove the link from the body after download starts
-    document.body.removeChild(downloadLink);
-  });
+/* ----- Hire Me button functionality ----- */
+document.getElementById("hire-me-btn").addEventListener("click", function () {
+  window.location.href = "mailto:ara.noor.ismot@gmail.com";
 });
 
-/*Tester Journal Button */
-function redirectToExternalLink() {
-  window.open("https://medium.com/@ara.noor.ismot", "_blank")
+
+
+/*-------------Download-CV button Functionality -------- */
+
+function initiateDownloadCV() {
+  // Create an anchor element
+  var downloadLink = document.createElement("a");
+
+  // Set the download link attributes
+  downloadLink.href = "assets/images/Noor-Ismot-Ara-CV.pdf"
+  downloadLink.download = 'Noor-Ismot-Ara-CV.pdf';
+
+  // Trigger a click on the anchor element
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
 }
 
-// Event listener for the button click
-document.getElementById("testerJournalBtn").addEventListener("click", redirectToExternalLink);
-
-
-/* ----- HIRE ME BUTTON FUNCTIONALITY ----- */
-const hireMeButton = document.getElementById('hireMeBtn');
-
-hireMeButton.addEventListener('click', function () {
-  // Add my email address
-  const email = 'ara.noor.ismot@gmail.com';
-
-  // Create the mailto link
-  const mailtoLink = `mailto:${email}`;
-
-  // Open the user's email client
-  window.location.href = mailtoLink;
-});
-
-// Add an event listener to each project-box
-const projectBoxes = document.querySelectorAll('.project-box');
-
-projectBoxes.forEach((box) => {
-  box.addEventListener('click', function () {
-    // Retrieve the external link from the data-href attribute
-    const externalLink = this.getAttribute('data-href');
-
-    // Open the external link in a new tab
-    window.open(externalLink, '_blank');
-  });
+// Add event listeners for all buttons with the "download-cv" class
+var downloadButtons = document.querySelectorAll(".download-cv");
+downloadButtons.forEach(function (button) {
+  button.addEventListener("click", initiateDownloadCV);
 });
 
 
-// Scrolltop
+// Back to top button
 const scrolltop = document.getElementById("scrolltop");
 
 window.addEventListener("scroll", () => {
@@ -192,3 +174,44 @@ window.addEventListener("scroll", () => {
     scrolltop.classList.remove("scrolltop--show");
   }
 });
+
+/*------------Form Functions -------*/
+function initiateEmail() {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  // Get the form data
+  const formData = {
+    from_name: document.getElementById("name").value,
+    from_email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  // Send the email using EmailJS
+  emailjs.send('service_5ic9y38', 'template_1uxonis', formData)
+    .then(function (response) {
+      console.log('SUCCESS!', response.status, response.text);
+      alert('Your message has been sent successfully!');
+      document.getElementById("contact-form").reset(); // Reset the form after successful submission
+    }, function (error) {
+      console.log('FAILED...', error);
+      alert('Oops! Something went wrong. Please try again later.');
+    });
+}
+
+document.getElementById("contact-form").addEventListener("submit", initiateEmail);
+
+document.getElementById('submit-btn').addEventListener('click', function (event) {
+  var nameInput = document.querySelector('#contact-form input[type="text"]');
+  var emailInput = document.querySelector('#contact-form input[type="email"]');
+  var messageTextarea = document.querySelector('#contact-form textarea');
+  var requiredMessage = document.getElementById('required-message');
+
+  if (!nameInput.value || !emailInput.value || !messageTextarea.value) {
+    requiredMessage.style.display = 'block';
+    event.preventDefault(); // Prevent form submission
+  } else {
+    requiredMessage.style.display = 'none';
+    // If all fields are filled, submit the form (default behavior)
+  }
+});
+
